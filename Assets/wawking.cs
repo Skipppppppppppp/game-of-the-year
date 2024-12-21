5f7134693d1ae9e31038b732835cb61ace6172e9
AntonUSM-PRs <antonusm@proton.me>
2024-12-21 17:10:10 +0200

    made guys figure out platform colliders

--- Assets/wawking.cs
+++ Assets/wawking.cs
@@ -6,32 +6,59 @@ using UnityEngine;
 
 public class wawking : MonoBehaviour
 {
-    public BoxCollider2D platformCollider;
     private Vector2 destination;
     private Rigidbody2D rb2d;
+    private BoxCollider2D guyCollider;
+    private bool guyFoundCollider;
     private float waxX;
     private float winX;
     [Range(0.01f,10)] public float speed;
 
     void SelectDestination(float minX, float maxX, float currentY)
     {
-        float newX = Random.Range(minX, maxX);
+        float newX = Random.Range(minX+guyCollider.bounds.size.x, maxX-guyCollider.bounds.size.x);
         Vector2 newDestination = new Vector2(newX, currentY);
         destination = newDestination;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+
+        // cast
+        // BoxCollider2D boxCollider = (BoxCollider2D) collider;
+
+        // // as
+        // BoxCollider2D? boxCollider = collider as BoxCollider2D;
+        // if (boxCollider != null)
+        // {
+        //     // 
+        // }
+
+        if (collider is not BoxCollider2D boxCollider)
+        {
+            Debug.Log("guy trying to walk on wrong collider");
+            return;
+        }
+
+        var winX = boxCollider.bounds.min.x;
+        var waxX = boxCollider.bounds.max.x;
+        var winY = boxCollider.bounds.min.y;
+        var worldPositionTopLeft = new Vector2(winX, winY);
+        var worldPositionTopRight = new Vector2(waxX, winY);
+        this.winX = worldPositionTopLeft.x;
+        this.waxX = worldPositionTopRight.x;
+        guyFoundCollider = true;
+        destination = transform.position;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
         destination = transform.position;
-        var winX = platformCollider.bounds.min.x;
-        var waxX = platformCollider.bounds.max.x;
-        var winY = platformCollider.bounds.min.y;
-        var worldPositionTopLeft = new Vector2(winX, winY);
-        var worldPositionTopRight = new Vector2(waxX, winY);
-        this.winX = worldPositionTopLeft.x;
-        this.waxX = worldPositionTopRight.x;
+        guyFoundCollider = false;
+        rb2d = GetComponent<Rigidbody2D>();
+        guyCollider = GetComponentInChildren<BoxCollider2D>();
         StartCoroutine(UpdateCoroutine());
     }
 
@@ -39,10 +66,15 @@ public class wawking : MonoBehaviour
     {
         while (true)
         {
+            if (guyFoundCollider == false)
+            {
+                yield return null;
+                continue;
+            }
             if (Mathf.Abs(destination.x - transform.position.x) <= 0.1f)
             {
                 SelectDestination(winX, waxX, transform.position.y);
-                float waitingTime = Random.Range(1f,3f);
+                float waitingTime = Random.Range(3f,5f);
                 yield return new WaitForSeconds(waitingTime);
                 continue;
             }
